268396ba6ea9dae00af38e85b4c2d9e52a459746
Pranav Kumar <pranavkumar1522@gmail.com>
2024-03-05 13:02:05 +0530

    changes in about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
           <Info />
 
           <p className="about__description">
-            Frontend developer, I create web pages with UI / UX user interface,
+            Full stack developer, I create web pages with UI / UX user interface,
             I have just started this experience and carried out some projects.
           </p>
           <a download="" href={CV} className="button button--flex">
